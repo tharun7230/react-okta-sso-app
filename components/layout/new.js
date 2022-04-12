99f658196bb38cb2f0b247164082ea4328049615
Tharun Kumar <84845064+tharun7230@users.noreply.github.com>
2022-04-12 12:30:01 +0530

    added files
    added files

---
+++ components/layout/new.js
@@ -0,0 +1,15 @@
+
+let startDate = '2020-10-10'
+let endDate = '2020-10-20'
+
+
+function myFunc(startDate, endDate) {
+  newStartDate = parseInt(startDate);
+  newEndDate = parseInt(endDate);
+
+  console.log(newEndDate)
+  for (i = 0; startDate > endDate; i++) {
+    
+  }
+  
+}
